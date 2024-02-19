bca72cabcfee4183e595e2ac6ff2cae9e6d65616
Findof <joeargon21@gmail.com>
2024-02-19 12:57:15 -0500

    no null chatroom

--- app/components/CustomRoomJoin.js
+++ app/components/CustomRoomJoin.js
@@ -5,16 +5,26 @@ import Button from "./Button"
 
 const CustomRoomJoin = () => {
   const [room, setRoom] = useState('')
+  const [route, setRoute] = useState('')
+
 
   useEffect(()=>{
     setRoom(sessionStorage.getItem('roomState'))
   },[])
 
+  useEffect(()=>{
+    if(room){
+      setRoute(`/chat/${room}`)
+    }else{
+      setRoute('/chat')
+    }
+
+  }, [room])
 
   return (
     <div className="flex flex-row items-center mt-10 ">
       <TextInput label='Custom Room Name: ' extraStyles="fade-in-4" value={room} onChange={(e)=>{setRoom(e.target.value); sessionStorage.setItem('roomState', e.target.value);}}></TextInput>
-      <Button route={`/chat/${room}`} extraStyles='fade-in-5 ml-2'>Join Room</Button>
+      <Button route={route} extraStyles='fade-in-5 ml-2'>Join Room</Button>
     </div>
   )
 }
