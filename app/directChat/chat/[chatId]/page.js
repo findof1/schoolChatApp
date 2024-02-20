95be70f8f6ea7d958eba838680ccd97f0dcf49a1
Findof <joeargon21@gmail.com>
2024-02-20 15:43:03 -0500

    screen sizing fix

--- app/directChat/chat/[chatId]/page.js
+++ app/directChat/chat/[chatId]/page.js
@@ -8,12 +8,15 @@ export default function Home({params}) {
 
 
   return (
-    <div className="flex flex-col items-center text-white h-full w-full">
-      <NavbarAuth userdata={null}></NavbarAuth>
+    <>
+    <NavbarAuth userdata={null}></NavbarAuth>
+    <div className="flex flex-col items-center text-white h-[90%] w-full">
+      
       <div className="flex flex-row h-full w-full">
       <Messages chat={params.chatId} path="direct"></Messages>
       <Users id={params.chatId}/>
       </div>
     </div>
+    </>
   );
 }
