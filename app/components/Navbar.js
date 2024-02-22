7491a02856ec894a3bc42bd8c0bcfb5bbf080e8a
Findof <joeargon21@gmail.com>
2024-02-22 15:35:36 -0500

    shows online users

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,15 +1,14 @@
 'use client';
-import Link from 'next/link'
 import React from 'react'
 
 const Navbar = ({homeRedirect, children}) => {
   return (
     <nav className='w-screen h-[10vh] border-b-8 border-black bg-gray-700 flex flex-row items-center text-white pl-6 nav-fade-in'>
-      <Link href={homeRedirect} onClick={()=>{sessionStorage.clear();}} className='text-2xl'>Home</Link>
+      <a href={homeRedirect} onClick={()=>{sessionStorage.clear();}} className='text-2xl'>Home</a>
       <div className='w-1 h-[100%] bg-gray-800 ml-7'></div>
-      <Link href='/chat' onClick={()=>{sessionStorage.clear();}} className='text-2xl ml-7'>Chatrooms</Link>
+      <a href='/chat' onClick={()=>{sessionStorage.clear();}} className='text-2xl ml-7'>Chatrooms</a>
       <div className='w-1 h-[100%] bg-gray-800 ml-7'></div>
-      <Link href='/directChat' onClick={()=>{sessionStorage.clear();}} className='text-2xl ml-7'>Direct Chats</Link>
+      <a href='/directChat' onClick={()=>{sessionStorage.clear();}} className='text-2xl ml-7'>Direct Chats</a>
       <div className='w-1 h-[100%] bg-gray-800 ml-7'></div>
       <div className='flex flex-row items-center ml-auto mr-5'>
         {children}
--- app/components/Users.js
+++ app/components/Users.js
@@ -1,10 +1,14 @@
 'use client'
 import React, { useCallback, useEffect, useState } from 'react'
-import { db } from '../firebase-config';
+import { db, rl } from '../firebase-config';
 import { collection, documentId, getDocs, query, where } from 'firebase/firestore';
+import { off, onDisconnect, onValue, ref, remove, set } from 'firebase/database';
+import { useUser } from "@clerk/clerk-react";
 
 const Users = ({id}) => {
+  const {user} = useUser()
   const [users, setUsers] = useState([])
+  const [online, setOnline] = useState([])
 
   const fetchData = useCallback(async()=>{
     const q = query(
@@ -20,12 +24,56 @@ const Users = ({id}) => {
   useEffect(()=>{
     fetchData()
   }, [fetchData])
+
+  useEffect(() => {
+    if(user){
+    const userRef = ref(rl, `connectedUsers/${id}/${user.id}`);
+    const chatConnRef = ref(rl, `connectedUsers/${id}`)
+    const handleConnectionStateChange = (connected) => {
+      if (connected) {
+
+
+        set(userRef, user.emailAddresses[0].emailAddress); 
+
+        
+        onDisconnect(userRef).remove();
+      }
+    };
+
+    const connectedRef = ref(rl, '.info/connected');
+    onValue(connectedRef, (snapshot) => {
+      handleConnectionStateChange(snapshot.val());
+    });
+
+    const handleConnectedUsersChange = (snapshot) => {
+      const connectedUsers = snapshot.val();
+      const usersList = connectedUsers ? Object.values(connectedUsers) : [];
+      setOnline(usersList);
+    };
+
+    onValue(chatConnRef, handleConnectedUsersChange);
+    return () => {
+      off(connectedRef, 'value', handleConnectionStateChange);
+      off(chatConnRef, 'value', handleConnectedUsersChange);
+    };
+  }
+  }, [user]);
+
+
   return (
     <div className='w-[20%] mt-[6%] h-[85%] bg-gray-950 border-8 border-gray-800 text-xl p-4 flex flex-col overflow-auto'>
-      <h1 className='underline text-xl text-left'>Users</h1>
+
+      <h1 className='underline text-xl text-left'>Online Users</h1>
+      {online.map((user, index)=>(
+        <p className='text-md' key={index}>{user}</p>
+      ))}
+
+      <h1 className='underline text-xl text-left'>All Users</h1>
       {users.map((user, index)=>(
         <p className='text-md' key={index}>{user}</p>
       ))}
+
+
     </div>
   )
 }
